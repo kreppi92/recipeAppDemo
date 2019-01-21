4cbe418fb02158c01ce0cb0743866f64c341f117
Stefan Kovac <mali_stefan@msn.com>
2019-01-20 19:51:26 -0500

    fixed linting errors and ran npm prebuild

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -15,6 +15,7 @@ export const saveState = (state) => {
     const serializedState = JSON.stringify(state)
     localStorage.setItem('state', serializedState)
   } catch (err) {
+    alert(err)
   }
 }
 
